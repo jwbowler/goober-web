16228c640a7406d0930a931ce43c995787e09875
Ubuntu <ubuntu@ip-172-31-0-38.us-west-2.compute.internal>
2015-09-25 04:21:31 +0000

    "hello world" express web server

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+var express = require('express');
 var redis = require('redis');
+
+
+// Setup Redis
 
 var redisHost = 'john-redis.2wlafm.ng.0001.usw2.cache.amazonaws.com';
 var redisPort = 6379;
@@ -6,7 +10,7 @@ var redisPort = 6379;
 var client = redis.createClient(redisPort, redisHost);
 
 client.on('connect', function() {
-    console.log('connected');
+    console.log('Connected to Redis');
 });
 
 client.on('message', function (channel, message) {
@@ -17,3 +21,19 @@ client.subscribe('loc_0_p90');
 client.subscribe('loc_1_p90');
 client.subscribe('loc_2_p90');
 client.subscribe('loc_3_p90');
+
+
+// Setup webserver
+
+var app = express();
+
+app.get('/', function (req, res) {
+      res.send('Hello World!');
+});
+
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('App listening at http://%s:%s', host, port);
+});
