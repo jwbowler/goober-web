afe06c1340af5996f6985db7c76cb156e2cbb17e
Ubuntu <ubuntu@ip-172-31-0-38.us-west-2.compute.internal>
2015-09-25 04:11:23 +0000

    server printing 4 percentiles

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var redis = require('redis');
 
-var redisHost = 'localhost';
+var redisHost = 'john-redis.2wlafm.ng.0001.usw2.cache.amazonaws.com';
 var redisPort = 6379;
 
 var client = redis.createClient(redisPort, redisHost);
 
 client.on('connect', function() {
     console.log('connected');
-
-    setInterval(function() {
-        client.get("test", function(err, reply) {
-            console.log(reply);
-        });
-    }, 1000)
 });
+
+client.on('message', function (channel, message) {
+    console.log(channel + ": " + message)
+});
+
+client.subscribe('loc_0_p90');
+client.subscribe('loc_1_p90');
+client.subscribe('loc_2_p90');
+client.subscribe('loc_3_p90');
